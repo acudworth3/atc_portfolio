9170d5c3344e10a4941a5179c6f382e2220a6cbc
Cudworth, Andrew Thomas <acudworth5@gatech.edu>
2022-08-14 19:56:18 -0500

    small change

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Andrew has a website
         </a>
       </header>
     </div>
