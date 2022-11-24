dc9823cdd82968fb7e93982ebc4961604856c81c
Cudworth, Andrew Thomas <acudworth5@gatech.edu>
2022-11-23 19:47:01 -0600

    misc changes

--- src/App.js
+++ src/App.js
@@ -7,7 +7,8 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit test
+           <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
@@ -15,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Andrew has a website
+          Learn React
         </a>
       </header>
     </div>
