75780ea9fd9abc4ee20c07fb1489ccab92df82d7
Cudworth, Andrew Thomas <acudworth5@gatech.edu>
2022-11-25 14:58:05 -0600

    setup routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,7 @@
 import "./App.css";
-import React from "react";
-import { BrowserRouter as Router, Route ,Link, Routes} from "react-router-dom";
-import Page1 from "./features/pages/page1";
-import Page2 from "./features/pages/page2";
+import { BrowserRouter as Router, Route ,Routes} from "react-router-dom";
+import Page1 from "./features/pages/Page1";
+import Page2 from "./features/pages/Page2";
 import Page3 from "./features/pages/Page3";
 
 export default function App() {
@@ -10,10 +9,10 @@ export default function App() {
 <div className="App">
 <Router>
 <Routes>
-    <Route exact path="/" element={<h1>Home Page</h1>} />
-    <Route exact path="page1" element={<Page1 />} />
-    <Route exact path="page2" element={<Page2 />} />
-    <Route exact path="page3" element={<Page3 />} />
+    <Route  path="/atc_portfolio" element={<h1>Home Page</h1>} />
+    <Route  path="page1" element={<Page1 />} />
+    <Route  path="page2" element={<Page2 />} />
+    <Route  path="page3" element={<Page3 />} />
   </Routes>
 </Router>
 </div>
