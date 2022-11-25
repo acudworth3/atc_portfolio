cb422d1e561ecb75cb81606f37e11ebbae9c0886
Cudworth, Andrew Thomas <acudworth5@gatech.edu>
2022-11-25 10:17:36 -0600

    setup routing; checkpoint

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,22 @@
 import "./App.css";
-import {GridTableSample} from './features/ag-grid-sample/GridTableSample'
+import React from "react";
+import { BrowserRouter as Router, Route ,Link, Routes} from "react-router-dom";
+import Page1 from "./features/pages/page1";
+import Page2 from "./features/pages/page2";
+import Page3 from "./features/pages/Page3";
 
-function App() {
-  return (<>
-      <GridTableSample></GridTableSample>      
-    </>
+export default function App() {
+  return (
+<div className="App">
+<Router>
+<Routes>
+    <Route exact path="/" element={<h1>Home Page</h1>} />
+    <Route exact path="page1" element={<Page1 />} />
+    <Route exact path="page2" element={<Page2 />} />
+    <Route exact path="page3" element={<Page3 />} />
+  </Routes>
+</Router>
+</div>
   );
 }
 
-export default App;
