c9e7914a12be8fffc00cd7b710bc8e12713425e8
Cudworth, Andrew Thomas <acudworth5@gatech.edu>
2022-12-21 20:18:24 -0600

    full spread

--- src/features/counter/Counter.tsx
+++ src/features/counter/Counter.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 import { useAppSelector, useAppDispatch } from "../../app/hooks";
 import {
@@ -8,62 +8,83 @@ import {
   incrementAsync,
   incrementIfOdd,
   selectCount,
+  modifyNestedObj,
 } from "./counterSlice";
 import styles from "./Counter.module.css";
+import { Box } from "@mui/material";
 
 export function Counter() {
   const count = useAppSelector(selectCount);
   const { nestObj } = useAppSelector((state) => state.counter);
   const dispatch = useAppDispatch();
   const [incrementAmount, setIncrementAmount] = useState("2");
+  const [displayObj, setdisplayObj] = useState(
+    useAppSelector((state) => state.counter.nestObj)
+  );
 
   const incrementValue = Number(incrementAmount) || 0;
 
+  useEffect(() => {
+    console.log(nestObj);
+  }, [nestObj]);
+
   return (
-    <div>
-      <div className={styles.row}>
-        <button
-          className={styles.button}
-          aria-label="Decrement value"
-          onClick={() => dispatch(decrement())}
-        >
-          -
-        </button>
-        <span className={styles.value}>{count}</span>
-        <button
-          className={styles.button}
-          aria-label="Increment value"
-          onClick={() => dispatch(increment())}
-        >
-          +
-        </button>
+    <>
+      <div>
+        <div className={styles.row}>
+          <button
+            className={styles.button}
+            aria-label="Decrement value"
+            onClick={() => dispatch(decrement())}
+          >
+            -
+          </button>
+          <span className={styles.value}>{count}</span>
+          <button
+            className={styles.button}
+            aria-label="Increment value"
+            onClick={() => dispatch(increment())}
+          >
+            +
+          </button>
+        </div>
+        <div className={styles.row}>
+          <input
+            className={styles.textbox}
+            aria-label="Set increment amount"
+            value={incrementAmount}
+            onChange={(e) => setIncrementAmount(e.target.value)}
+          />
+          <button
+            className={styles.button}
+            onClick={() => dispatch(incrementByAmount(incrementValue))}
+          >
+            Add Amount
+          </button>
+          <button
+            className={styles.asyncButton}
+            onClick={() => dispatch(incrementAsync(incrementValue))}
+          >
+            Add Async
+          </button>
+          <button
+            className={styles.button}
+            onClick={() => dispatch(incrementIfOdd(incrementValue))}
+          >
+            Add If Odd
+          </button>
+          <button
+            className={styles.button}
+            onClick={() =>
+              dispatch(
+                modifyNestedObj({ bool1: true, bool2: true, bool3: true })
+              )
+            }
+          >
+            Demo Generic reducer
+          </button>
+        </div>
       </div>
-      <div className={styles.row}>
-        <input
-          className={styles.textbox}
-          aria-label="Set increment amount"
-          value={incrementAmount}
-          onChange={(e) => setIncrementAmount(e.target.value)}
-        />
-        <button
-          className={styles.button}
-          onClick={() => dispatch(incrementByAmount(incrementValue))}
-        >
-          Add Amount
-        </button>
-        <button
-          className={styles.asyncButton}
-          onClick={() => dispatch(incrementAsync(incrementValue))}
-        >
-          Add Async
-        </button>
-        <button
-          className={styles.button}
-          onClick={() => dispatch(incrementIfOdd(incrementValue))}
-        >
-          Add If Odd
-        </button>
-      </div>
-    </div>
+    </>
   );
 }
--- src/features/counter/counterSlice.ts
+++ src/features/counter/counterSlice.ts
@@ -69,7 +69,7 @@ export const counterSlice = createSlice({
   },
 });
 
-export const { increment, decrement, incrementByAmount } = counterSlice.actions;
+export const { increment, decrement, incrementByAmount, modifyNestedObj } = counterSlice.actions;
 
 // The function below is called a selector and allows us to select a value from
 // the state. Selectors can also be defined inline where they're used instead of
